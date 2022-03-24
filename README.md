5f6ba55e5885f1ebc1fdd53d38a0e09dcdeae602
hacker <agarwalbinay19@gmail.com>
2022-03-24 19:40:25 +0530

    notification-toast

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+## Installation & Set Up
+
+1. Install project dependencies
+```bash
+  yarn install or npm install
+```
+
+2. Start the development server
+```bash
+  yarn dev or npm run dev
+```
+
+3. Build for production
+
+```bash
+  yarn build or npm run build
+```
+
+5. Run production mode
+
+```bash
+  yarn start or npm start
+```
+
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import Button from './components/button/Button'
+import Toast from './components/toast/Toast'
+import styles from './App.module.css'
+
+function App() {
+  const [list, setList] = useState([]);
+  let toastProperties = null;
+
+  const showToast = type => {
+    switch(type) {
+      case 'success':
+        toastProperties = {
+          id: list.length+1,
+          title: 'Success',
+          description: 'Do you want to confirm',
+          backgroundColor: '#5cb85c'
+        }
+        break;
+      case 'error':
+        toastProperties = {
+          id: list.length+1,
+          title: 'Error',
+          description: 'Oops! An Error Occured',
+          backgroundColor: '#d9534f'
+        }
+        break;
+      case 'info':
+        toastProperties = {
+          id: list.length+1,
+          title: 'Info',
+          description: 'This is a info toast component',
+          backgroundColor: '#5bc0de'
+        }
+        break;
+      default:
+        toastProperties = [];
+    }
+    setList([...list, toastProperties]);
+  };
+
+  return (
+    <div className={styles.root}>
+      <h1>Notification Toast</h1>
+      <div className={styles.buttons}>
+        <Button handleClick={() => showToast('success')}>Success</Button>
+        <Button handleClick={() => showToast('error')}>Error</Button>
+        <Button handleClick={() => showToast('info')}>Info</Button>
+      </div>
+      <Toast toastlist={list} position="buttom-right" setList={setList} />
+    </div>
+  );
+}
+
+export default App;
